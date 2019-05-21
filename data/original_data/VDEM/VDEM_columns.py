743122d4bebb6fbbf6230ca2962d70524c541116
Sanittawan Tan <sanittawan.t@gmail.com>
2019-05-20 23:21:28 -0500

    Added VDEM

---
+++ data/original_data/VDEM/VDEM_columns.py
@@ -0,0 +1,59 @@
+### VDEM indicator selection
+
+
+nominal = ['v2exl_legitideolcr_0', 'v2exl_legitideolcr_1', 
+            'v2exl_legitideolcr_2', 'v2exl_legitideolcr_3',
+            'v2exl_legitideolcr_4', 'v2lgfunds', 'v2lgamend',
+            'v2exhoshog', 'v2exaphogp', 'v2exaphos', 'v2ddlexci',
+            'v2ddlexrf', 'v2ddlexpl'
+]
+
+ordinal = ['v2exl_legitlead_ord', 'v2pepwrgeo_ord', 'v2clgencl_ord',
+            'v2clpolcl_ord', 'v2peapssoc_ord', 'v2peapsgen_ord',
+            'v2peapsecon_ord', 'v2peapspol_ord', 'v2peapsgeo_ord',
+            'v2pepwrses_ord', 'v2pepwrsoc_ord', 'v2pepwrgen_ord',
+            'v2peedueq_ord', 'v2pehealth_ord', 'v2mecenefm_ord',
+            'v2mecenefi_ord', 'v2mecrit_ord', 'v2meslfcen_ord',
+            'v2mebias_ord','v2mecorrpt_ord','v2cseeorgs_ord',
+            'v2csreprss_ord', 'v2clrelig_ord'
+            'v2clacfree_ord', 'v2cltrnslw_ord', 'v2clrspct_ord',
+            'v2cldiscm_ord','v2cldiscw_ord','v2jureform_ord',
+            'v2jupoatck_ord', 'v2jupack_ord', 'v2juhcind_ord',
+            'v2juncind_ord', 'v2jureview_ord', 'v2lgotovst_ord',
+            'v2lgoppart_ord', 'v2exremhsp_ord',
+            'v2exremhog_ord', 'v2exrescon_ord', 'v2exbribe_ord',
+            'v2psbars_ord', 'v2psoppaut_ord', 'v2psplats_ord',
+            'v2pscohesv_ord', 'v2eldonate_ord', 'v2elpubfin_ord',
+            'v2elembaut_ord', 'v2elmulpar_ord', 'v2elvotbuy_ord',
+            'v2elfrcamp_ord', 'v2elfrfair_ord', 'v2elaccept_ord',
+            'v2elasmoff_ord', 'v2eltrnout_ord', 'v2elintim'
+]
+
+continuous = ['v2lginello', 'v2lginelup'
+]
+
+cols_to_keep = ['v2exl_legitideolcr_0', 'v2exl_legitideolcr_1', 
+            'v2exl_legitideolcr_2', 'v2exl_legitideolcr_3',
+            'v2exl_legitideolcr_4', 'v2lgfunds', 'v2lgamend',
+            'v2exhoshog', 'v2exaphogp', 'v2exaphos', 'v2ddlexci',
+            'v2ddlexrf', 'v2ddlexpl', 'v2lginello', 'v2lginelup',
+            'v2exl_legitlead_ord', 'v2pepwrgeo_ord', 'v2clgencl_ord',
+            'v2clpolcl_ord', 'v2peapssoc_ord', 'v2peapsgen_ord',
+            'v2peapsecon_ord', 'v2peapspol_ord', 'v2peapsgeo_ord',
+            'v2pepwrses_ord', 'v2pepwrsoc_ord', 'v2pepwrgen_ord',
+            'v2peedueq_ord', 'v2pehealth_ord', 'v2mecenefm_ord',
+            'v2mecenefi_ord', 'v2mecrit_ord', 'v2meslfcen_ord',
+            'v2mebias_ord','v2mecorrpt_ord','v2cseeorgs_ord',
+            'v2csreprss_ord', 'v2clrelig_ord'
+            'v2clacfree_ord', 'v2cltrnslw_ord', 'v2clrspct_ord',
+            'v2cldiscm_ord','v2cldiscw_ord','v2jureform_ord',
+            'v2jupoatck_ord', 'v2jupack_ord', 'v2juhcind_ord',
+            'v2juncind_ord', 'v2jureview_ord', 'v2lgotovst_ord',
+            'v2lgoppart_ord', 'v2exremhsp_ord',
+            'v2exremhog_ord', 'v2exrescon_ord', 'v2exbribe_ord',
+            'v2psbars_ord', 'v2psoppaut_ord', 'v2psplats_ord',
+            'v2pscohesv_ord', 'v2eldonate_ord', 'v2elpubfin_ord',
+            'v2elembaut_ord', 'v2elmulpar_ord', 'v2elvotbuy_ord',
+            'v2elfrcamp_ord', 'v2elfrfair_ord', 'v2elaccept_ord',
+            'v2elasmoff_ord', 'v2eltrnout_ord', 'v2elintim'
+]
